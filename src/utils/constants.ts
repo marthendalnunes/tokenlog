2d9779f825162ed65f8f3bc76bd2a89016a00028
Vitor Marthendal <marthendalnunes@gmail.com>
2022-07-19 14:57:08 -0300

    hotfix: change revalidate time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,7 +6,7 @@ export const IMAGE_URL = 'https://tokenlog.xyz/icon.png'
 
 export const CREATOR_NOTICE = '2020 — wslyvh'
 
-export const DEFAULT_CACHE_REVALIDATE = 600
+export const DEFAULT_CACHE_REVALIDATE = 10
 
 export const PRIMARY_COLOR = '#0366d6'
 export const SECONDARY_COLOR = '#2f363d'
